daacd474123a2153204c6c16db61b5ec2fa0bc8e
typical-developer047 <abdullah.ibn.shareef12@gmail.com>
2024-09-07 22:18:24 +0530

    Restored app1.py

---
+++ app1.py
@@ -0,0 +1,19 @@
+def exampleFunction():
+    letters = list(range(65, 75))
+    for index, num in enumerate(letters):
+        print(index, num)
+
+
+def sortList(givenList):
+    print(sorted(givenList, reverse=False))
+    givenList.sort(reverse=True)
+    print(givenList)
+
+
+exampleFunction()
+sendList = list(range(1, 20))
+sortList(sendList)
+print("Hello ")
+print("world")
+print("changes")
+print("restore")
